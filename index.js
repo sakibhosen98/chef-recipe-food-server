a2a8b5faedd52ffe955b2e4914a7ccd6c4b22e5b
sakibhosen98 <sakibhosen9815@gmail.com>
2023-05-04 13:22:21 +0600

    added some image

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
-const news = require('./data/news.json');
+// const news = require('./data/news.json');
 
 app.use(cors());
 
@@ -14,16 +14,6 @@ app.get('/', (req, res) =>{
 
 app.get('/categories', (req, res) =>{
   res.send(categories);
-})
-
-app.get('/news', (req, res) =>{
-  res.send(news)
-})
-
-app.get('/news/:id', (req, res) => {
-  const id = req.params.id;
-  const selectedNews = news.find(n => n.id === id)
-  res.send(selectedNews);
 })
 
 app.listen(port, () =>{
